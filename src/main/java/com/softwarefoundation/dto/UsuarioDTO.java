07921866e549e6d14fe25639959eaff4b7ec9a38
Dherkyan Ribeiro <dherkyan@gmail.com>
2022-09-26 00:52:27 -0300

    [Issue-1] Configurando lombok no projeto

--- src/main/java/com/softwarefoundation/dto/UsuarioDTO.java
+++ src/main/java/com/softwarefoundation/dto/UsuarioDTO.java
@@ -1,23 +1,11 @@
 package com.softwarefoundation.dto;
 
+import lombok.Data;
+
+@Data
 public class UsuarioDTO {
 
     private String nome;
     private Integer idade;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
+    
 }
